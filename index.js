ae61d4c7bf50cedcfc1593a3cf17652088b9e610
MdEstiakAhmed <estiak97@gmail.com>
2020-06-01 19:13:02 +0600

    change

--- index.js
+++ index.js
@@ -10,21 +10,17 @@ require('dotenv').config();
 let app = express();
 // instance end
 
+
 // project middleware start
 app.use(cors());
 app.use(bodyParser.json());
 // project middleware end
 
-// test declaration start
-// var test = require('./controllers/testController/test');
-// test declaration end
-
-// var test_data = require.main.require('./models/test');
 
 app.get('/testData', (req, res) =>{
     client = new MongoClient(process.env.DB_PATH, { useNewUrlParser: true }, { useUnifiedTopology: true });
     client.connect(err => {
-        const collection = client.db("hospital").collection("test");
+        const collection = client.db("power-x-gym").collection("test");
         collection.find().toArray((err, documents)=>{
             if(err){
                 console.log(err)
@@ -38,11 +34,6 @@ app.get('/testData', (req, res) =>{
     });
 });
 
-
-// test middleware start
-// app.use('/', test);
-// app.use('/testData', test);
-// test middleware end
 
 //server setup start
 var serverPort=3000;
