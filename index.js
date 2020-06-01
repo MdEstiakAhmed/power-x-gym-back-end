5477f8e7ee137eda609a209fdc68382e5c4d5130
MdEstiakAhmed <estiak97@gmail.com>
2020-06-01 20:16:27 +0600

    ck

--- index.js
+++ index.js
@@ -10,34 +10,43 @@ require('dotenv').config();
 let app = express();
 // instance end
 
-
 // project middleware start
 app.use(cors());
 app.use(bodyParser.json());
 // project middleware end
 
+// test declaration start
+var test = require('./controllers/testController/test');
+// test declaration end
 
-app.get('/testData', (req, res) =>{
-    client = new MongoClient(process.env.DB_PATH, { useNewUrlParser: true }, { useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("power-x-gym").collection("test");
-        collection.find().toArray((err, documents)=>{
-            if(err){
-                console.log(err)
-                res.status(500).send({message:err});
-            }
-            else{
-                res.send(documents);
-            }
-        });
-        client.close();
-    });
-});
+// var test_data = require.main.require('./models/test');
+
+// app.get('/testData', (req, res) =>{
+//     client = new MongoClient(process.env.DB_PATH, { useNewUrlParser: true }, { useUnifiedTopology: true });
+//     client.connect(err => {
+//         const collection = client.db("hospital").collection("test");
+//         collection.find().toArray((err, documents)=>{
+//             if(err){
+//                 console.log(err)
+//                 res.status(500).send({message:err});
+//             }
+//             else{
+//                 res.send(documents);
+//             }
+//         });
+//         client.close();
+//     });
+// });
+
+
+// test middleware start
+app.use('/', test);
+app.use('/testData', test);
+// test middleware end
 
 
 //server setup start
-var serverPort=3000;
-const host = '0.0.0.0';
+var serverPort=5000;
 app.listen(process.env.PORT || 5000, function(){
     console.log("project server started at: ", serverPort);
 });
